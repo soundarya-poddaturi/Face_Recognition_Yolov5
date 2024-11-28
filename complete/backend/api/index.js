ea7684bd51d6769140cf021f5cf9a77d8baaa987
soundarya-poddaturi <soundarya2873@gmail.com>
2024-11-28 11:26:20 +0530

    suppa

--- complete/backend/api/index.js
+++ complete/backend/api/index.js
@@ -9,7 +9,7 @@ mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
-
+console.log("wsefg");
 // Define User model
 const User = mongoose.model("User", new mongoose.Schema({
     name: String,
